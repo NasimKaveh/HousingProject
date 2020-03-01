57f9401024ba971566791c9d43357ecd84004f17
NasimKaveh <nas.kaveh@gmail.com>
2020-03-01 10:36:25 -0800

    useless file removed and environment.yml added

--- Price_prediction_NK.py
+++ Price_prediction_NK.py
@@ -3,6 +3,7 @@ import numpy as np
 import xgboost
 from xgboost import plot_importance
 from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score,KFold
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
@@ -11,16 +12,13 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
 from string_to_int import preprocess
 import matplotlib.pyplot as plt
+from sklearn.cross_validation import cross_val_score, cross_val_predict
+from sklearn import metrics
 
 df_train = preprocess(df=pd.read_csv('train.csv'))
 df_train.to_numpy()
 y_set_train = (np.array(df_train.SalePrice)).reshape((1460,1))
 x_set_train = (np.array(df_train.drop(['SalePrice'], axis = 1))).reshape((1460, 61))
-
-df_test = preprocess(df=pd.read_csv('test.csv'))
-df_test.to_numpy()
-y_set_test = (np.array(df_train.SalePrice)).reshape((1460,1))
-x_set_test = (np.array(df_train.drop(['SalePrice'], axis = 1))).reshape((1460, 61))
 
 
 #To normalize the dataset
@@ -28,27 +26,28 @@ scaler = MinMaxScaler(feature_range=(0, 1))
 x_set_train = scaler.fit_transform(x_set_train)
 y_set_train = scaler.fit_transform(y_set_train)
 
-x_set_test = scaler.fit_transform(x_set_test)
-y_set_test = scaler.fit_transform(y_set_test)
+
+#Linear regression
+X_train, X_test, y_train, y_test = train_test_split(x_set_train, y_set_train, random_state=0)
+lm = LinearRegression()
+linreg = lm.fit(X_train, y_train)
+predictions = lm.predict(X_test)
+plt.scatter(y_test, predictions)
+print ("Score LinReg:", linreg.score(X_test, y_test))
 
 #kfold
 scores = []
 best_svr = SVR(kernel='rbf')
-cv = KFold(n_splits=10, random_state=42, shuffle=False)
+cv = KFold(n_splits=10, random_state=42, shuffle=True)
 for train_index, test_index in cv.split(x_set_train):
     print("Train Index: ", train_index, "\n")
     print("Test Index: ", test_index)
-
     X_train, X_test, y_train, y_test = x_set_train[train_index], x_set_train[test_index], y_set_train[train_index], y_set_train[test_index]
     best_svr.fit(X_train, y_train)
     scores.append(best_svr.score(X_test, y_test))
-
 print(np.mean(scores))
 
 
-#Linear regression
-X_train, X_test, y_train, y_test = train_test_split(x_set_train, y_set_train, random_state=0)
-linreg = LinearRegression().fit(X_train, y_train)
 
 #XGBoost Regression
 best_xgb_model = xgboost.XGBRegressor(colsample_bytree=0.4,
@@ -63,6 +62,5 @@ best_xgb_model = xgboost.XGBRegressor(colsample_bytree=0.4,
                  seed=42)
 best_xgb_model.fit(x_set_train, y_set_train)
 
-df_test['Prediction'] = np.expm1(best_xgb_model.predict(x_set_test))
-filename = 'submission.csv'
-pd.DataFrame({'Id': df_test.Id, 'SalePrice': df_test.Prediction}).to_csv(filename, index=False)
+results = cross_val_score(best_xgb_model, x_set_train, y_set_train, cv=cv)
+print("Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
